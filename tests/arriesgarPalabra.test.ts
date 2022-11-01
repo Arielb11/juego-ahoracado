a965a08f20cbaead7d46bf3ad36239e498bffdaf
vmerlino <104107341+vmerlino@users.noreply.github.com>
2022-11-01 19:52:33 -0300

    Update arriesgarPalabra.test.ts

--- tests/arriesgarPalabra.test.ts
+++ tests/arriesgarPalabra.test.ts
@@ -26,7 +26,7 @@ describe('validar letra y restar vida', () => {
       expect(ahorcadoJuego.adivinarLetra('p')).toBe(false);
    })
    test('la letra ingresada no esta contenida en la palabra. se resta una vida', () => {
-      expect(ahorcadoJuego.vidas).toBe(8);
+      expect(ahorcadoJuego.vidas).toBe(5);
    })
    test('el jugador perdió', () => {
       ahorcadoJuego.vidas = 0;
